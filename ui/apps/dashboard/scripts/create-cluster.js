26e2a8152c2d8f47b073355bd37ff73e094696f6
warjiang <1096409085@qq.com>
2024-05-13 14:33:11 +0800

    feat: add ui monorepo
    Signed-off-by: warjiang <1096409085@qq.com>

---
+++ ui/apps/dashboard/scripts/create-cluster.js
@@ -0,0 +1,9 @@
+const data = {
+    "member_cluster_kubeconfig": `**`,
+    "sync_mode": "pull",
+    "member_cluster_name": "memberx",
+    "member_cluster_endpoint": "https://192.168.10.7:6443",
+    "member_cluster_namespace": "example"
+}
+
+console.log(JSON.stringify(data))
